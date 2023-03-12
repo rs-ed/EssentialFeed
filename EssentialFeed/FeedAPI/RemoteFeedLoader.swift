93e3c87d5fbdb25c1ab708942d5957cbc1cafe5c
Roland Schmitz <ed@2rs.de>
2023-03-12 22:01:31 +0100

    add RemoteFeedItem data transfer representation to decouple the FeedItemsMapper from the FeedItem data model

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -21,10 +21,19 @@ public final class RemoteFeedLoader: FeedLoader {
             guard self != nil else { return }
             switch result {
             case .success(let data, let response):
-                completion(FeedItemsMapper.map(data, from: response))
+                completion(Self.map(data, from: response))
             case .failure(_):
                 completion(.failure(Error.connectivity))
             }
+        }
+    }
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
         }
     }
 
@@ -33,5 +42,13 @@ public final class RemoteFeedLoader: FeedLoader {
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
+    }
+}
+
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        map {
+            FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
+        }
     }
 }
